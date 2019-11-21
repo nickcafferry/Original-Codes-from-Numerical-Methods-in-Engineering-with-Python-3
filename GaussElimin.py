c993a1c2d8a50f9318c688f42e0e629c351203a0
Wei Mei <56918345+nickcafferry@users.noreply.github.com>
2019-11-22 06:12:26 +0800

    Update GaussElimin.py

--- GaussElimin.py
+++ GaussElimin.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # # Welcome to Jupyter!
-
-# In[18]:
-
-
 from numpy import dot
-
-
-# In[19]:
-
-
 from numpy import array
-
-
-# In[3]:
-
 
 def gaussElimin(a,b):
     n = len(b)
@@ -30,45 +16,17 @@ def gaussElimin(a,b):
         b[k] = (b[k]-dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
     return b
 
-
-# In[20]:
-
-
 a = array([[1,1,1,1],[0,1,-1,-1],[0,0,-5,-8],[0,0,0,11]])
-
-
-# In[21]:
-
 
 b = array([[10],[-5],[-47],[44]])
 
-
-# In[22]:
-
-
 gaussElimin(a,b)
-
-
-# In[23]:
-
 
 a = array([[1,-2,2],[2,-3,-3],[4,1,6]])
 
-
-# In[24]:
-
-
 b = array([[-2],[4],[3]])
 
-
-# In[25]:
-
-
 gaussElimin(a,b)
-
-
-# In[ ]:
-
 
 from numpy import dot
 from numpy import array
